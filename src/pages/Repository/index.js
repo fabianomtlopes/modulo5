886e14f5d07ede1fe9c905b9125265af376a8539
Fabiano de Matos Lopes <fabianomtlopes@yahoo.com.br>
2019-12-04 15:47:47 -0300

    modulo05

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -4,22 +4,22 @@ import PropTypes from 'prop-types';
 import api from '../../services/api';
 
 import Container from '../../components/Container';
-import { Loading, Owner } from './styles';
+import { Loading, Owner, IssueList } from './styles';
 
 export default class Repository extends Component {
-  // static propTypes = {
-  //   match: PropTypes.shape({
-  //     params: PropTypes.shape({
-  //       repository: PropTypes.string,
-  //     }),
-  //   }).isRequired,
-  // };
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
 
-  // state = {
-  //   repository: {},
-  //   issues: [],
-  //   loading: true,
-  // };
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
 
   async componentDidMount() {
     const { match } = this.props;
@@ -27,43 +27,55 @@ export default class Repository extends Component {
     const repoName = decodeURIComponent(match.params.repository);
 
     const [repository, issues] = await Promise.all([
-      api.get(`/repos/${repoName}`),
-      api.get(`/repos/${repoName}/issues`),
-      // , {
-      //   params: {
-      //     state: 'open',
-      //     per_page: 5,
-      //   },
-      // }),
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
     ]);
 
-    console.log(repository);
-    // console.log(issues);
-
-    // this.setState({
-    //   repository: repository.data,
-    //   issues: issues.data,
-    //   loading: false,
-    // });
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
   }
 
   render() {
-    // const { repository, issues, loading } = this.state;
+    const { repository, issues, loading } = this.state;
 
-    // if (loading) {
-    //   return <Loading>Carregando</Loading>;
-    // }
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
 
     return (
-      <h1>Repository</h1>
-      // <Container>
-      //   <Owner>
-      //     <Link to="/">Voltar aos Repositórios</Link>
-      //     <img src={repository.owner.avatar_url} alt={repository.owner.login} />
-      //     <h1>{repository.name}</h1>
-      //     <p>{repository.description}</p>
-      //   </Owner>
-      // </Container>
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos Repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+
+        <IssueList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
+      </Container>
     );
   }
 }
